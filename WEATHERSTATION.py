3db2bde71bf9043655f82bb368236905ddaaef8d
CMGrandy <cmgrandy42@tntech.edu>
2024-12-03 19:31:30 -0600

    Add files via upload

--- WEATHERSTATION.py
+++ WEATHERSTATION.py
@@ -1,7 +1,12 @@
 from machine import Pin, SoftI2C
 import ssd1306
-from time import sleep
+import time 
 import dht
+cur_time = time.localtime()
+
+filename = str(cur_time[0]) + "_" +str(cur_time[1]) + "_" + str(cur_time[2]) + "_" + str(abs(cur_time[3]-12)) + "_" + str(cur_time[4]) + "_" + str(cur_time[5])
+filename = "Weather_Station" + filename + ".txt"
+file = open(filename,"w")
 
 led1_pin = 18
 led2_pin = 19
@@ -18,9 +23,22 @@ oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
+def saveData(tempC, tempF, Hum):
+    cur_time = time.localtime()
+    raw_time = (int(cur_time[3]) * 3600 ) + (int(cur_time[4]) * 60) + int(cur_time[5])
+    file.write(str(raw_time) + " ")
+    file.write("Temp C: "+ str(tempC))
+    file.write(" Temp F: " + str(tempF))
+    file.write(" Humidity(%): " + str(Hum))
+    file.write("\n")
+
+
+
+
+
 while True:
   try:
-    sleep(1)
+    
     sensor.measure()
     temp = sensor.temperature()
     hum = sensor.humidity()
@@ -48,6 +66,8 @@ while True:
     temp_string = str(temp)
     hum_string = str(hum)
     temp_f_string = str(temp_f)
+    oled.fill(0)
+    oled.show()
     oled.text("WEATHER STATION" ,0 ,0)
     oled.text("DEG C:", 0, 30)
     oled.text("DEG F:", 0, 40)
@@ -56,11 +76,11 @@ while True:
     oled.text(temp_f_string, 45, 40)
     oled.text(hum_string, 45, 50)
     oled.show()
-    sleep(10)
-    oled.fill(0)
-    oled.show()
+    saveData(temp, temp_f, hum)
+    time.sleep(10)
+
   except OSError as e:
     oled.text('Failed to read sensor.')
     oled.show()
-    
+
 
